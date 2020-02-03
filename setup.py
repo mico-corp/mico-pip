513c6f61ef46a6857b5e9e0747ebea6ace3724a5
Bardo91 <19Bardo91@gmail.com>
2020-02-03 12:58:29 +0100

    updated version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mico-pip',
-    version='0.1.0',
+    version='0.1.1',
     description='MICO package installation module',
     url='https://github.com/mico-corp/mico-pip',
     author='Pablo Ramon Soria',
